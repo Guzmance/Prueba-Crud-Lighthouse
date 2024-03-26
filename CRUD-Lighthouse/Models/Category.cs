fa014f0760d644a71b2a5bf5fa5927df05da3e1e
Carlos Guzmán <carlose.guzmane62@hotmail.com>
2024-03-26 16:32:34 -0600

    Add: Category Model

---
+++ CRUD-Lighthouse/Models/Category.cs
@@ -0,0 +1,9 @@
+﻿namespace CRUD_Lighthouse.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Image { get; set; }
+    }
+}
